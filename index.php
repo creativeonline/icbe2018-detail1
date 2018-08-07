a67aa22dfef561e095d9eb496b96453a11caa5e6
Creative Online <jo.creativeonline@gmail.com>
2018-08-07 18:34:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-hello
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>ICBC</title>
+       </head>
+    <body>
+      <a href="http://www.isuzucreativebrainschallenge.com/site/detail"><img src="detail1.jpg"></a>
+    </body>
+</html>
